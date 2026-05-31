f4b28140c8c9ed8e3d2c3cc5d550a8dcbb033b2b
Syed Ibtehaj Ahsan <sibtehajahsan@gmail.com>
2026-05-31 23:56:14 +0500

    Delete Assignment-6 directory

--- Assignment-6/cypress/pages/ProductPage.js
+++
@@ -1,9 +0,0 @@
-class ProductPage {
-  getProductName() {
-    return cy.get('.inventory_details_name');
-  }
-  getProductPrice() {
-    return cy.get('.inventory_details_price');
-  }
-}
-export default new ProductPage();
